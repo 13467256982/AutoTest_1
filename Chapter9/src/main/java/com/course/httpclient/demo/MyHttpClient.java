f8facd31cc332a3e7c69f6f1edc19ca2276bdd03
YangNa <13467256982@163.com>
2018-08-24 17:41:04 +0800

    +

--- Chapter9/src/main/java/com/course/httpclient/demo/MyHttpClient.java
+++ Chapter9/src/main/java/com/course/httpclient/demo/MyHttpClient.java
@@ -1,9 +1,17 @@
 package com.course.httpclient.demo;
 
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.Test;
 
@@ -21,6 +29,10 @@ public class MyHttpClient {
         HttpResponse reponse = client.execute(get);
         result = EntityUtils.toString(reponse.getEntity(),"utf-8");
         System.out.println(result);
+
+
+
+
 
 
     }
